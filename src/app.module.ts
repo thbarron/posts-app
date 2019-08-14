ba16ea5b444f3a9f69b9f866efa1fb982f4fd327
Tyree Barron <tyree.barron@gmail.com>
2019-08-13 21:57:09 -0400

    added mongodb to configuration and set up sandbox account to begin

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://posts_db_user:postsPW1@ds261817.mlab.com:61817/posts_dev_db',
+      { useNewUrlParser: true },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
