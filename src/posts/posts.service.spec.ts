7e4190164696d53f2d1fa1d2da6b2ff493dd28f8
Tyree Barron <tyree.barron@gmail.com>
2019-10-22 07:23:34 -0400

    began tests with post service

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,16 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { ObjectID } from "mongodb";
+
 import { PostsService } from './posts.service';
+import { Post } from '../entities/post.entity';
 
 describe('PostsService', () => {
   let service: PostsService;
+  let repository: Repository<Post>;
+
+  const testId = new ObjectID('5dae3b35f869f63048619cb9')
+
+  const testPost: Post = {
+    id: testId,
+    name: 'test post name',
+    contents: 'test contents for post'
+  };
+
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [PostsService,
+        {
+          provide: getRepositoryToken(Post),
+          useClass: Repository,
+        }],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+    repository = module.get<Repository<Post>>(getRepositoryToken(Post))
   });
 
-  it.todo('should be defined');
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('should return post(s) for findAll', async () => {
+    jest.spyOn(repository, 'find').mockResolvedValueOnce([testPost]);
+    expect(await service.getAll()).toEqual([testPost])
+  })
 });
